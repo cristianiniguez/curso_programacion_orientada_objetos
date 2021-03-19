b24ee6213c6f880e5f57be881e4544d1ad6eb4d2
cristianiniguez <felizpc1@gmail.com>
2021-03-18 21:10:53 -0400

    clases, objetos y metodo constructor: objetos - dando vida a nuestras clases en java y python

--- java/Main.java
+++ java/Main.java
@@ -1,5 +1,15 @@
 class Main {
   public static void main(String[] args) {
-    System.out.println("Hola Mundo");
+    Car car1 = new Car();
+    car1.license = "AMQ123";
+    car1.driver = "Andrés Herrera";
+    car1.passenger = 4;
+    car1.printDataCar(); // License: AMQ123; Driver: Andrés Herrera
+
+    Car car2 = new Car();
+    car2.license = "QWE567";
+    car2.driver = "Andrea Herrera";
+    car2.passenger = 3;
+    car2.printDataCar(); // License: QWE567; Driver: Andrea Herrera
   }
 }
--- python/main.py
+++ python/main.py
@@ -1,2 +1,14 @@
+from car import Car
+
 if __name__ == "__main__":
-    print("Hola Mundo")
+    car1 = Car()
+    car1.license = "AMQ123"
+    car1.driver = "Andrés Herrera"
+    car1.passenger = 4
+    print(vars(car1))  # {'license': 'AMQ123', 'driver': 'Andrés Herrera', 'passenger': 4}
+
+    car2 = Car()
+    car2.license = "QWE567"
+    car2.driver = "Andrea Herrera"
+    car2.passenger = 3
+    print(vars(car2))  # {'license': 'QWE567', 'driver': 'Andrea Herrera', 'passenger': 3}
