187f3c904590164ceb118ed9d0ff5554a4459b71
cristianiniguez <felizpc1@gmail.com>
2021-03-18 21:48:05 -0400

    clases, objetos y metodo constructor: declarando un metodo constructor en java y javascript

--- java/Main.java
+++ java/Main.java
@@ -1,15 +1,15 @@
 class Main {
   public static void main(String[] args) {
-    Car car1 = new Car();
-    car1.license = "AMQ123";
-    car1.driver = "Andrés Herrera";
+    Car car1 = new Car("AMQ123", new Account("Andrés Herrera", "AND123"));
+    // car1.license = "AMQ123";
+    // car1.driver = "Andrés Herrera";
     car1.passenger = 4;
-    car1.printDataCar(); // License: AMQ123; Driver: Andrés Herrera
+    car1.printDataCar(); // License: AMQ123; Driver name: Andrés Herrera
 
-    Car car2 = new Car();
-    car2.license = "QWE567";
-    car2.driver = "Andrea Herrera";
+    Car car2 = new Car("QWE567", new Account("Andrea Herrera", "AND456"));
+    // car2.license = "QWE567";
+    // car2.driver = "Andrea Herrera";
     car2.passenger = 3;
-    car2.printDataCar(); // License: QWE567; Driver: Andrea Herrera
+    car2.printDataCar(); // License: QWE567; Driver name: Andrea Herrera
   }
 }
--- js/index.js
+++ js/index.js
@@ -1,1 +1,10 @@
-console.log('Hola Mundo');
+const Car = require('./Car');
+const Account = require('./Account');
+
+var car1 = new Car('AW456', new Account('Andres Herrera', 'AND123'));
+car1.passenger = 4;
+
+car1.printDataCar();
+// Account { name: 'Andres Herrera', document: 'AND123' }
+// Andres Herrera
+// AND123
