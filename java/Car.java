6bf302fc95cab1bfa6dee1597efa052317819e52
cristianiniguez <felizpc1@gmail.com>
2021-03-20 15:06:14 -0400

    encapsulamiento: introduccion

--- java/Car.java
+++ java/Car.java
@@ -10,6 +10,6 @@ class Car {
   }
 
   void printDataCar() {
-    System.out.println("License: " + license + "; Driver name: " + driver.name);
+    System.out.println("License: " + license + "; Driver name: " + driver.name + "; Passengers: " + passenger);
   }
 }
--- java/Main.java
+++ java/Main.java
@@ -1,15 +1,7 @@
 class Main {
   public static void main(String[] args) {
-    Car car1 = new Car("AMQ123", new Account("Andrés Herrera", "AND123"));
-    // car1.license = "AMQ123";
-    // car1.driver = "Andrés Herrera";
-    car1.passenger = 4;
-    car1.printDataCar(); // License: AMQ123; Driver name: Andrés Herrera
-
-    Car car2 = new Car("QWE567", new Account("Andrea Herrera", "AND456"));
-    // car2.license = "QWE567";
-    // car2.driver = "Andrea Herrera";
-    car2.passenger = 3;
-    car2.printDataCar(); // License: QWE567; Driver name: Andrea Herrera
+    UberX uberX = new UberX("AMQ123", new Account("Andrés Herrera", "AND123"), "Chevrolet", "Sonic");
+    uberX.passenger = 3;
+    uberX.printDataCar(); // License: AMQ123; Driver name: Andrés Herrera Passengers: 3
   }
 }
