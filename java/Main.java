e64b2b1e71d1b0575194cef647d1045ec7b338a9
cristianiniguez <felizpc1@gmail.com>
2021-03-20 15:23:01 -0400

    encapsulamiento: encapsulando atributos en java

--- java/Main.java
+++ java/Main.java
@@ -1,7 +1,7 @@
 class Main {
   public static void main(String[] args) {
     UberX uberX = new UberX("AMQ123", new Account("Andrés Herrera", "AND123"), "Chevrolet", "Sonic");
-    uberX.passenger = 3;
-    uberX.printDataCar(); // License: AMQ123; Driver name: Andrés Herrera Passengers: 3
+    uberX.setPassenger(3);
+    uberX.printDataCar(); // License: AMQ123; Driver name: Andrés Herrera Passengers: null
   }
 }
