4d92190f1b4a8c5dea2311574c3d5d02c9a6f948
cristianiniguez <felizpc1@gmail.com>
2021-03-19 20:30:02 -0400

    herencia: aplicando herencia en lenguaje python y javascript

---
+++ js/UberPool.js
@@ -0,0 +1,11 @@
+const Car = require('./Car');
+
+class UberPool extends Car {
+  constructor(license, driver, brand, model) {
+    super(license, driver);
+    this.brand = brand;
+    this.model = model;
+  }
+}
+
+module.exports = UberPool;
--- js/index.js
+++ js/index.js
@@ -1,10 +1,17 @@
 const Car = require('./Car');
+const UberX = require('./UberX');
 const Account = require('./Account');
 
 var car1 = new Car('AW456', new Account('Andres Herrera', 'AND123'));
 car1.passenger = 4;
-
 car1.printDataCar();
 // Account { name: 'Andres Herrera', document: 'AND123' }
 // Andres Herrera
 // AND123
+
+var uberX = new UberX('AW456', new Account('Andrea Ferran', 'ANDA765'), 'Chevrolet', 'Spark');
+uberX.passenger = 4;
+uberX.printDataCar();
+// Account { name: 'Andrea Ferran', document: 'ANDA765' }
+// Andrea Ferran
+// ANDA765
