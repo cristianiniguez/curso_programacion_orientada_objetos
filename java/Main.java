e153da7ba20412aa8e7251162eccf20e4f977031
cristianiniguez <felizpc1@gmail.com>
2021-03-20 15:57:50 -0400

    polimorfismo: generando polimorfismo en java

--- java/Main.java
+++ java/Main.java
@@ -1,7 +1,13 @@
 class Main {
   public static void main(String[] args) {
     UberX uberX = new UberX("AMQ123", new Account("Andrés Herrera", "AND123"), "Chevrolet", "Sonic");
-    uberX.setPassenger(3);
-    uberX.printDataCar(); // License: AMQ123; Driver name: Andrés Herrera Passengers: null
+    uberX.setPassenger(4);
+    uberX.printDataCar(); // ... Passengers: 4
+
+    UberVan uberVan = new UberVan("FGH345", new Account("Andrés Herrera", "AND123"));
+    uberVan.setPassenger(6); // Todo bien
+    uberVan.printDataCar();
+    // Model: Sonic; Brand: Chevrolet
+    // License: FGH345; Driver name: Andrés Herrera
   }
 }
